2b71de08622911caf0326135fd8886fe647e2282
Anthony <alwaysadevzero@gmail.com>
2023-04-10 06:46:38 +0400

    feat: add burger menu for index and pets pages

--- shelter/assets/js/burger.js
+++ shelter/assets/js/burger.js
@@ -1,8 +1,18 @@
-const burgerMenuButton = document.querySelector(".header__burger-menu");
-const logo = document.querySelector(".header__logo");
-const burgerMenu = document.querySelector(".header__menu");
+const burgerMenuButton = document.querySelector(".header__burger-icon");
+const burgerCover = document.querySelector(".header__burger-cover");
+const burgerMenu = document.querySelector(".header__burger-menu");
+const scroll = document.querySelector(".page");
 
-burgerMenuButton.addEventListener("click", function() {
-    logo.classList.toggle("active");
+const burgerItems = document.querySelectorAll(".burger__link");
+
+burgerItems.forEach(item => item.addEventListener("click", burgerToggler));
+burgerMenuButton.addEventListener("click", burgerToggler);
+burgerCover.addEventListener("click", burgerToggler);
+
+
+function burgerToggler() {
+    burgerCover.classList.toggle("active");
     burgerMenu.classList.toggle("active");
-});
+	burgerMenuButton.classList.toggle("active");
+	scroll.classList.toggle("scroll-disable");
+}
